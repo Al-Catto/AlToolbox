46b0123c35427fe34127d0cba7b84f753c4a00d8
Alistair Catto <alistaircatto@googlemail.com>
2021-07-29 21:00:01 +0100

    correcting tests

--- AlToolbox/weather_city_search.py
+++ AlToolbox/weather_city_search.py
@@ -1,6 +1,7 @@
 import sys
 import urllib.parse
 import requests
+from math import radians, cos, sin, asin, sqrt
 
 BASE_URI = "https://www.metaweather.com"
 """Check URL response"""
@@ -17,7 +18,21 @@ def search_city(query):
         return None  
 
 
-from mlproject.distance import haversine
+def haversine(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance between two points
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    # haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    c = 2 * asin(sqrt(a))
+    r = 6371  # Radius of earth in kilometers. Use 3956 for miles
+    return c * r
 
 if __name__ == "__main__":
     # Le Wagon location
